50d09836d83f30d6fcfe70a7b51e41419d94f862
MixFix7 <mashtalar95@gmail.com>
2023-06-11 16:06:11 +0300

    debug UTF-8 problem

--- UkraineAlarmsApi/ApiApp/models.py
+++ UkraineAlarmsApi/ApiApp/models.py
@@ -1,32 +1,58 @@
 from django.db import models
 
+
 class UkraineAlarmsStatus(models.Model):
     datetime = models.DateTimeField(auto_now_add=True)
-    Вінницька = models.BooleanField()
-    Волинська = models.BooleanField()
-    Дніпропетровська = models.BooleanField()
-    Донецька = models.BooleanField()
-    Житомирська = models.BooleanField()
-    Закарпатська = models.BooleanField()
-    Запорізька = models.BooleanField()
-    Івано_Франківська = models.BooleanField()
-    Київська = models.BooleanField()
-    Кіровоградська = models.BooleanField()
-    Луганська = models.BooleanField()
-    Львівська = models.BooleanField()
-    Миколаївська = models.BooleanField()
-    Одеська = models.BooleanField()
-    Полтавська = models.BooleanField()
-    Рівненська = models.BooleanField()
-    Сумська = models.BooleanField()
-    Тернопільська = models.BooleanField()
-    Харківська = models.BooleanField()
-    Херсонська = models.BooleanField()
-    Хмельницька = models.BooleanField()
-    Черкаська = models.BooleanField()
-    Чернігівська = models.BooleanField()
-    Чернівецька = models.BooleanField()
-    Автономна_Республіка_Крим = models.BooleanField()
+    Vinnytsia = models.BooleanField()
+    Volyn = models.BooleanField()
+    Dnipropetrovska = models.BooleanField()
+    Donetsk = models.BooleanField()
+    Zhytomyr = models.BooleanField()
+    Transcarpathian = models.BooleanField()
+    Zaporizhzhya = models.BooleanField()
+    Ivano_Frankivsk = models.BooleanField()
+    Kyiv = models.BooleanField()
+    Kirovograd = models.BooleanField()
+    Luhansk = models.BooleanField()
+    Lviv = models.BooleanField()
+    Mykolaiv = models.BooleanField()
+    Odesa = models.BooleanField()
+    Poltava = models.BooleanField()
+    Rivne = models.BooleanField()
+    Sumy = models.BooleanField()
+    Ternopil = models.BooleanField()
+    Kharkiv = models.BooleanField()
+    Kherson = models.BooleanField()
+    Khmelnytsky = models.BooleanField()
+    Cherkasy = models.BooleanField()
+    Chernihiv = models.BooleanField()
+    Chernivtsi = models.BooleanField()
+    Crimea = models.BooleanField()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     def __str__(self):
         return f"Status of alarms in Ukraine on {self.datetime}"
