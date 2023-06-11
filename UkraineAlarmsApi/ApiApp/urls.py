781adf88a420f4f8ab7c28715043b231ec31c7a1
MixFix7 <mashtalar95@gmail.com>
2023-06-11 15:50:39 +0300

    update api

--- UkraineAlarmsApi/ApiApp/urls.py
+++ UkraineAlarmsApi/ApiApp/urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 from .api import UkraineViewSet
 
 router = routers.DefaultRouter()
-router.register('api/ukrainealarms/', UkraineViewSet, 'Ukraine')
+router.register('api/ukrainealarms', UkraineViewSet, 'Ukraine')
 
 urlpatterns = router.urls
---
+++ test.py
@@ -0,0 +1,7 @@
+import requests
+from pprint import pprint
+
+data = requests.request('GET', 'http://127.0.0.1:8000/api/ukrainealarms/')
+
+pprint(data.content)
+
