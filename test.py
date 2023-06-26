3427bc5fe14dfe0c87bdf34162b908a45d74860d
MixFix7 <mashtalar95@gmail.com>
2023-06-26 23:14:00 +0300

    update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
-from pprint import pprint
+import pprint
 
 data = requests.request('GET', 'http://127.0.0.1:8000/api/ukraineApi/')
 
-pprint(data.json())
+pprint.pprint(data.json())
